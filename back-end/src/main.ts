3936d88d904a6e9148dedfa69792d910e1b7b432
nickgabriadze <nickgabriadze@gmail.com>
2024-08-03 16:22:32 +0400

    feat: defined routes for get and post requests

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,16 @@ const app = express()
 app.use(express.json())
 
 
+app.get('/profile', (req: express.Request, res: express.Response) => {
+
+})
+
+
+app.post('/login', (req: express.Request, res: express.Response) => {
+
+})
+
+
 app.listen('3000', () => {
     console.log('Server started on port 3000')
 })
