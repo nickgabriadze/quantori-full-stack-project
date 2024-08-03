6740f7c2feba830fe55b2382f5de26e9d88a55dd
nickgabriadze <nickgabriadze@gmail.com>
2024-08-03 14:49:48 +0400

    feature: implemented login page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,15 @@
+import Header from "./components/header/Header.tsx";
+import Footer from "./components/footer/Footer.tsx";
+import LoginContainer from "./components/login/LoginContainer.tsx";
 
 
-function App(){
+function App() {
 
+    return <>
+        <Header/>
+        <LoginContainer />
+        <Footer />
+    </>
 
 }
 
---
+++ front-end/src/components/login/LoginContainer.tsx
@@ -0,0 +1,49 @@
+import AuthorizationCSS from './authorization.module.css'
+import HeaderCSS from './../header/header.module.css'
+import {useState} from "react";
+
+export function LoginContainer() {
+    const [authInfo, setAuthInfo] = useState({
+        email: '',
+        password: ''
+    })
+
+
+
+    const clearForm = () => {
+        setAuthInfo({email: '', password: ''})
+    }
+
+    return <form className={AuthorizationCSS['login-form']}>
+        <h1>Login</h1>
+
+        <div className={AuthorizationCSS['authorization-input-container']}>
+            <label htmlFor={"email"}>Email</label>
+            <input
+                value={authInfo.email}
+                autoComplete={'true'}
+                onChange={(e) => setAuthInfo({...authInfo, email: e.target.value})}
+                type={'email'} id={'email'} placeholder={'email@example.com'} maxLength={255} required/>
+        </div>
+
+        <div className={AuthorizationCSS['authorization-input-container']}>
+            <label htmlFor={'password'}>Password</label>
+            <input
+                autoComplete={'true'}
+                value={authInfo.password}
+                onChange={(e) => setAuthInfo({...authInfo, password: e.target.value})}
+                type={'password'} name={'password'} id={'password'} maxLength={255} required/>
+        </div>
+
+        <div className={AuthorizationCSS['cancel-submit']}>
+            <button type={"button"}
+                    name={'password'}
+                    onClick={clearForm}
+                    className={AuthorizationCSS['cancel-btn']}>Cancel</button>
+            <button className={HeaderCSS['login-btn']}>Login</button>
+        </div>
+    </form>
+
+}
+
+export default LoginContainer;
