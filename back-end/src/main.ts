d9bf3e94e0561bad6d04875777ae59b452510fe0
nickgabriadze <nickgabriadze@gmail.com>
2024-08-03 20:24:20 +0400

    feat: created POST request api for 'login' functionality

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,21 +1,47 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
+import checkUserInDatabase from "./utils/checkUserInDatabase";
+import generateToken from "./utils/generateToken";
+import {User} from "./types/user";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/profile', (req: express.Request, res: express.Response) => {
-
-})
-
-
-app.post('/login', (req: express.Request, res: express.Response) => {
-
-})
+app.get('/profile', async (req: express.Request, res: express.Response) => {
 
 
-app.listen('3000', () => {
-    console.log('Server started on port 3000')
-})
+});
+
+app.post('/login', async (req: express.Request, res: express.Response) => {
+    try {
+        const {email, password} = req.body;
+
+
+        const user: User = await checkUserInDatabase('./db/users.csv', email, password);
+
+        if (!user) {
+            res.json({
+                status: 403,
+                message: 'User not found'
+            })
+        } else {
+            const token = await generateToken(user.id, user.email);
+            res.json({
+                status: 200,
+                message: token
+            })
+
+        }
+    } catch (error) {
+        res.send({
+            status: 500,
+            message: 'Internal Server Error',
+        });
+    }
+});
+
+app.listen(3001, () => {
+    console.log('Server started on port 3001');
+});
