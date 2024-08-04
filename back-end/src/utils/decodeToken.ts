620bda27b7bf212f395e3abd867d41f419769bf6
nickgabriadze <nickgabriadze@gmail.com>
2024-08-04 15:11:39 +0400

    change: updated the ways tokens are generated and decoded

--- back-end/src/utils/decodeToken.ts
+++ back-end/src/utils/decodeToken.ts
@@ -1,7 +1,18 @@
-import jose from 'jose'
+import {decodeJwt} from 'jose'
+
 export const decodeToken = (token: string) => {
 
-    console.log(jose.decodeJwt(token))
+    try {
+        const decoded =  decodeJwt(token)
+        if (Math.floor((new Date().getTime() / 1000)) > decoded.exp){
+            return undefined
+        }else{
+            return decoded
+        }
+    } catch (err) {
+        return undefined
+    }
+
 }
 
 export default decodeToken;
--- back-end/src/utils/generateToken.ts
+++ back-end/src/utils/generateToken.ts
@@ -4,14 +4,14 @@ import {SignJWT} from "jose";
 dotenv.config()
 
 
-export const generateToken = async (id: number, email: string) => {
+export const generateToken = async (email: string) => {
     const secret = process.env.JWT_SECRET as string
 
     const encoder = new TextEncoder();
     const secretKey = encoder.encode(secret);
 
-    return await new SignJWT({ id, email })
-        .setExpirationTime("15m")
+    return await new SignJWT({ email })
+        .setExpirationTime("1m")
         .setIssuedAt()
         .setProtectedHeader({ alg: "HS256" })
         .sign(secretKey);
