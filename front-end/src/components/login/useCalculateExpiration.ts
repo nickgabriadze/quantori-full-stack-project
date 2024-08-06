a6432008a02514343eb0e9e044d636f3b3d93915
nickgabriadze <nickgabriadze@gmail.com>
2024-08-06 15:37:22 +0400

    fix: now handling null values

--- front-end/src/components/login/useCalculateExpiration.ts
+++ front-end/src/components/login/useCalculateExpiration.ts
@@ -2,14 +2,19 @@ import {useEffect, useState} from "react";
 import {calculateTimeRemaining} from "./getTimeLeft.ts";
 import {decodeJwt} from "jose";
 
-export function useCalculateExpiration(token){
+export function useCalculateExpiration(token) {
     const [tillExpiry, setTillExpiry] = useState<string>('null');
+
 
     useEffect(() => {
 
         const oneSecondTimeout = setTimeout(() => {
-            setTillExpiry(calculateTimeRemaining(new Date(decodeJwt(token).exp * 1000).toISOString()))
+            if (!token) {
+                setTillExpiry('Expired')
+            } else {
+                setTillExpiry(calculateTimeRemaining(new Date(decodeJwt(token).exp * 1000).toISOString()))
 
+            }
         }, 1000)
 
 
