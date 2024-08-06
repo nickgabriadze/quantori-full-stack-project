d00f8d79326780db9c5a29bb9cb3a653fb5b4fa3
nickgabriadze <nickgabriadze@gmail.com>
2024-08-06 15:42:13 +0400

    feat: handling errors for log in(in case server is unresponsive)

--- front-end/src/components/login/LoginContainer.tsx
+++ front-end/src/components/login/LoginContainer.tsx
@@ -13,16 +13,19 @@ export function LoginContainer() {
     }
 
 
-
     const loginUser = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        const request = await login(authInfo.email, authInfo.password)
-        if (request.data.status === 403) {
-            alert("Incorrect email or password!")
-        }
-        if (request.data.status === 200) {
-            localStorage.setItem('accessToken', request.data.message)
-            window.dispatchEvent(new Event("user-auth"))
+        try {
+            const request = await login(authInfo.email, authInfo.password)
+            if (request.data.status === 403) {
+                alert("Incorrect email or password!")
+            }
+            if (request.data.status === 200) {
+                localStorage.setItem('accessToken', request.data.message)
+                window.dispatchEvent(new Event("user-auth"))
+            }
+        }catch(err){
+            alert(err.message)
         }
     }
 
@@ -32,23 +35,23 @@ export function LoginContainer() {
         <h1>Login</h1>
 
         <div className={AuthorizationCSS['input-wrapper']}>
-        <div className={AuthorizationCSS['authorization-input-container']}>
-            <label htmlFor={"email"}>Email</label>
-            <input
-                value={authInfo.email}
-                autoComplete={'true'}
-                onChange={(e) => setAuthInfo({...authInfo, email: e.target.value})}
-                type={'email'} id={'email'} placeholder={'email@example.com'} maxLength={255} required/>
-        </div>
+            <div className={AuthorizationCSS['authorization-input-container']}>
+                <label htmlFor={"email"}>Email</label>
+                <input
+                    value={authInfo.email}
+                    autoComplete={'true'}
+                    onChange={(e) => setAuthInfo({...authInfo, email: e.target.value})}
+                    type={'email'} id={'email'} placeholder={'email@example.com'} maxLength={255} required/>
+            </div>
 
-        <div className={AuthorizationCSS['authorization-input-container']}>
-            <label htmlFor={'password'}>Password</label>
-            <input
-                autoComplete={'true'}
-                value={authInfo.password}
-                onChange={(e) => setAuthInfo({...authInfo, password: e.target.value})}
-                type={'password'} name={'password'} id={'password'} maxLength={255} required/>
-        </div>
+            <div className={AuthorizationCSS['authorization-input-container']}>
+                <label htmlFor={'password'}>Password</label>
+                <input
+                    autoComplete={'true'}
+                    value={authInfo.password}
+                    onChange={(e) => setAuthInfo({...authInfo, password: e.target.value})}
+                    type={'password'} name={'password'} id={'password'} maxLength={255} required/>
+            </div>
         </div>
 
         <div className={AuthorizationCSS['cancel-submit']}>
